4ed6e980e5b947b9cbedac97b21bc1e81a08e3a1
geung <geungan9@gmail.com>
2023-03-29 15:07:34 +0900

    코딩 테스트 깃 추가

--- src/main/java/dfsbfs1/재귀피보나치수열.java
+++ src/main/java/dfsbfs1/재귀피보나치수열.java
@@ -1,16 +1,25 @@
 package dfsbfs1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class 재귀피보나치수열 {
+
+    static int[] arr = new int[11];
     public static int DFS(int n) {
+        if (arr[n] > 0) {
+            return arr[n];
+        }
         if (n == 1) {
-            return 1;
+            return arr[1] = 1;
         } else if (n == 2) {
-            return 1;
+            return arr[2] =1;
         } else {
-            return DFS(n - 1) + DFS(n - 2);
+            return arr[n] = DFS(n - 1) + DFS(n - 2);
         }
     }
     public static void main(String[] args) {
-        System.out.println(DFS(5));
+        DFS(10);
+        System.out.println(Arrays.toString(arr));
     }
 }
