c7074366a0cb710a3df7a3af7b01dcb6bafd7cf4
geung <geungan9@gmail.com>
2023-04-03 09:13:59 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/DFS2/중복순열.java
@@ -0,0 +1,28 @@
+package DFS2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 중복순열 {
+
+    static int n, m;
+    static int[] arr;
+
+    public static void DFS(int L) {
+        if (L == m) {
+            System.out.println(Arrays.toString(arr));
+        } else {
+            for (int i = 1; i <= n; i++) {
+                arr[L] = i;
+                DFS(L + 1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[m];
+        DFS(0);
+    }
+}
