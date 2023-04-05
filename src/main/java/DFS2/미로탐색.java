0fb9230b0f21102ab702694601f4cc0ddd360d83
geung <geungan9@gmail.com>
2023-04-05 14:30:54 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/DFS2/미로탐색.java
@@ -0,0 +1,5 @@
+package DFS2;
+
+public class 미로탐색 {
+
+}
--- src/main/java/DFS2/조합구하기.java
+++ src/main/java/DFS2/조합구하기.java
@@ -1,9 +1,31 @@
 package DFS2;
 
+import java.util.Scanner;
+
 public class 조합구하기 {
 
-    public static void main(String[] args) {
+    static int n, r;
+    //조합을 담을 배열
+    static int[] combi;
 
+    public static void DFS(int L, int s) {
+        if (L == r) {
+            for(int x : combi) System.out.print(x+" ");
+            System.out.println();
+        } else {
+            //DFS로 조합을 구하는 핵심 풀이
+            for (int i = s; i <= n; i++) {
+                combi[L] = i;
+                DFS(L + 1, i + 1);
+            }
+        }
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        r = sc.nextInt();
+        combi = new int[r];
+        DFS(0, 1);
+    }
 }
