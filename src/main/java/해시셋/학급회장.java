caae3a3d1f41d0db726dce047c6318b19a794c25
geung <geungan9@gmail.com>
2023-04-20 21:53:21 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/해시셋/학급회장.java
@@ -0,0 +1,38 @@
+package 해시셋;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class 학급회장 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        Map<String, Integer> map = new HashMap<>();
+//        map.put("A", 0);
+//        map.put("B", 0);
+//        map.put("C", 0);
+//        map.put("D", 0);
+//        map.put("E", 0);
+        String[] split = str.split("");
+        for (String s : split) {
+            Integer orDefault = map.getOrDefault(s, 0);
+            map.put(s, orDefault + 1);
+        }
+        int answer = 0;
+        String result = "";
+        for (String key : map.keySet()) {
+            if (answer < map.get(key)) {
+                answer= map.get(key);
+                result = key;
+            }
+        }
+        System.out.println(result);
+
+    }
+
+}
