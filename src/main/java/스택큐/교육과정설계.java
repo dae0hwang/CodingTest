486c6113113d1c6916e7b9e399c5818bff6c71d5
geung <geungan9@gmail.com>
2023-04-26 23:31:47 +0900

    코딩 테스트 깃 추가

--- src/main/java/스택큐/교육과정설계.java
+++ src/main/java/스택큐/교육과정설계.java
@@ -11,24 +11,27 @@ public class 교육과정설계 {
         String n = sc.next();
         String t = sc.next();
 
-        char[] chars = t.toCharArray();
         Queue<Character> queue = new LinkedList<>();
         for (char c : n.toCharArray()) {
             queue.add(c);
         }
-        System.out.println(queue);
-        char first = queue.peek();
-        for (char aChar : chars) {
-            if (first == aChar && !queue.isEmpty()) {
-                queue.poll();
-                first = queue.peek();
+
+        String answer = "YES";
+        for (char x : t.toCharArray()) {
+            if (queue.contains(x)) {
+                //듣는 과목이 큐 안에 있다면
+                // 가장 큐의 맨 앞 과목이어야 한다.
+                if (x != queue.poll()) {
+                    answer = "NO";
+                    break;
+                }
             }
         }
-        System.out.println(queue);
-        if (queue.isEmpty()) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
+        //큐가 비어져 있지 않다. 필수 과목을 듣지 않았다
+        //그러면 NO 출력
+        if (!queue.isEmpty()) {
+            answer = "NO";
         }
+        System.out.println(answer);
     }
 }
