5c751c9ef80d93695d8b8f0f96d4f81b23ada94f
geung <geungan9@gmail.com>
2023-05-04 23:41:43 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/백준DFSBFS/영역구하기2583.java
@@ -0,0 +1,22 @@
+package 백준DFSBFS;
+
+import java.util.Scanner;
+
+public class 영역구하기2583 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int[][] board = new int[n][m];
+        //주어진 값 반대로 해서  board 완성한다음에
+
+    }
+
+    public static void DFS(int[][] board) {
+        
+    }
+
+
+}
