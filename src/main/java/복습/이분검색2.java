f6c9eeaaadaded9cd9921511126c5a9347a55962
geung <geungan9@gmail.com>
2023-05-06 15:33:42 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/복습/이분검색2.java
@@ -0,0 +1,36 @@
+package 복습;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 이분검색2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int answer = 0;
+        int lt = 0;
+        int rt = arr.length;
+        while (lt <= rt) {
+            //lt가 커지는 순간 끝이다
+            int mid = (lt + rt) / 2;
+            if (arr[mid] > k) {
+                rt = mid;
+            } else if (arr[mid] < k) {
+                lt = mid;
+            } else {
+
+                answer = mid + 1;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
---
+++ src/main/java/복습/재귀함수2.java
@@ -0,0 +1,17 @@
+package 복습;
+
+public class 재귀함수2 {
+    public static void DFS(int n) {
+        if (n == 1) {
+            System.out.print(n+" ");
+        } else {
+            int nextInt = n % 2;
+            DFS(n/2 );                //6번 라인
+            System.out.print(nextInt + " "); //7번 라인
+        }
+    }
+    public static void main(String[] args){
+        DFS(11);
+    }
+
+}
