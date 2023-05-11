5719506a6de74c32629dcecd2c11055132d89617
geung <geungan9@gmail.com>
2023-05-12 00:20:40 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/백준스택큐/크게만들기.java
@@ -0,0 +1,45 @@
+package 백준스택큐;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class 크게만들기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        String str = sc.next();
+        char[] chars = str.toCharArray();
+        for (char aChar : chars) {
+            list.add(aChar - 48);
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (Integer integer : list) {
+            if (stack.isEmpty()) {
+                stack.add(integer);
+            } else {
+
+            }
+        }
+
+
+    }
+
+}
+
+
+class BackendEnginer {
+    String name, phoneNumber,
+}
+
+class 인재채용 {
+
+    BackendEngine 지원자 =
+
+}
