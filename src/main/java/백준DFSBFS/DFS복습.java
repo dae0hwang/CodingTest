d21ec729a6c0e59ec914b2e42a3ca321884ec32a
geung <geungan9@gmail.com>
2023-05-15 12:48:07 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/백준DFSBFS/DFS복습.java
@@ -0,0 +1,9 @@
+package 백준DFSBFS;
+
+public class DFS복습 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/main/java/백준스택큐/크게만들기.java
+++ src/main/java/백준스택큐/크게만들기.java
@@ -1,45 +1,40 @@
 package 백준스택큐;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class 크게만들기 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+        int n = sc.nextInt(); //총 받을 개수
+        int k = sc.nextInt(); //k개 지우기
+        int length = n - k;
+        String next = sc.next();
+        Integer[] arr = new Integer[n];
         List<Integer> list = new ArrayList<>();
-        String str = sc.next();
-        char[] chars = str.toCharArray();
-        for (char aChar : chars) {
-            list.add(aChar - 48);
+        for (char c : next.toCharArray()) {
+            list.add(c - 48);
         }
+        Integer[] integers = list.toArray(arr);
         Stack<Integer> stack = new Stack<>();
-        for (Integer integer : list) {
-            if (stack.isEmpty()) {
-                stack.add(integer);
-            } else {
-
+        for (int i = 0; i < integers.length; i++) {
+            while (k > 0 && !stack.isEmpty() && stack.peek() < integers[i]) {
+                stack.pop();
+                k--;
             }
+            stack.push(integers[i]);
         }
-
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(stack.get(i));
+        }
+        System.out.println(sb);
     }
-
 }
 
 
-class BackendEnginer {
-    String name, phoneNumber,
-}
 
-class 인재채용 {
-
-    BackendEngine 지원자 =
-
-}
