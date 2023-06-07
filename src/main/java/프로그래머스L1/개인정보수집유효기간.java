e59170ed5bfb7c60985c23a4fcd908513fcbd5a3
geung <geungan9@gmail.com>
2023-06-07 10:38:39 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/프로그래머스L1/개인정보수집유효기간.java
@@ -0,0 +1,8 @@
+package 프로그래머스L1;
+
+public class 개인정보수집유효기간 {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        int[] answer = {};
+        return answer;
+    }
+}
