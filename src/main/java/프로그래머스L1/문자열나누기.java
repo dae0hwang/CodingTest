936a76f6f474cd12c3fd79e38a7f8f009541f1eb
geung <geungan9@gmail.com>
2023-06-07 11:00:34 +0900

    코딩 테스트 깃 추가

---
+++ src/main/java/프로그래머스L1/문자열나누기.java
@@ -0,0 +1,36 @@
+package 프로그래머스L1;
+
+public class 문자열나누기 {
+    public int solution(String s) {
+        int answer = 0;
+        char[] chars = s.toCharArray();
+        boolean first = true;
+        Character f = null;
+        int a = 0;
+        int b = 0;
+        for (char aChar : chars) {
+            if (first) {
+                f = aChar;
+                first = false;
+                a++;
+            } else {
+                if (f.equals(aChar)) {
+                    a++;
+                } else {
+                    b++;
+                }
+                if (a == b) {
+                    answer++;
+                    first = true;
+                    a = 0;
+                    b = 0;
+                }
+            }
+        }
+        if (a != 0) {
+            answer++;
+        }
+        return answer;
+    }
+
+}
